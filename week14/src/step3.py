96d0c2a5ef7db429bd71d9806dc9587fca1f6913
Chris Fauerbach <chris.fauerbach@capitalone.com>
2020-11-19 17:03:30 -0500

    adding week 14

---
+++ week14/src/step3.py
@@ -0,0 +1,51 @@
+# Simple pygame program
+
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+
+# Import and initialize the pygame library
+import pygame
+
+# Setup the pygame engine
+pygame.init()
+
+
+# Define constants for the screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Create the screen object
+# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Run until the user asks to quit
+running = True
+
+# Main loop
+while running:
+    # Look at every event in the queue
+    for event in pygame.event.get():
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+
+        # Did the user click the window close button? If so, stop the loop.
+        elif event.type == QUIT:
+            running = False
+
+
+
+# Done! Time to quit.
+pygame.quit()
